be98ee923b48eb9e7e41c3889bbf5742a6d56a9f
ttahm3d <ttahm3d@gmail.com>
2022-04-01 19:23:41 +0530

    feat: exporting input and button

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Sidebar from "./Sidebar/Sidebar";
+import Input from "./Input/Input";
+import Button from "./Button/Button";
 
-export { Header, Footer, Sidebar };
+export { Header, Footer, Sidebar, Input, Button };
