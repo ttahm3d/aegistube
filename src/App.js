22b997079287b5140c857a01eeb2c1f18987fb0a
ttahm3d <ttahm3d@gmail.com>
2022-04-01 07:58:39 +0530

    feat: created base styles for page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Footer, Header, Sidebar } from "./components";
 import Router from "./router/Router";
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  console.log(window.innerWidth);
+
+  const toggleSidebar = () => setShowSidebar((s) => !s);
+
   return (
     <BrowserRouter>
       <div className="page">
-        <Header />
-        <Router />
+        <Header toggleSidebar={toggleSidebar} />
+        <div className="container">
+          <div className="main-container">
+            <Sidebar />
+            <Router />
+          </div>
+        </div>
         <Footer />
       </div>
     </BrowserRouter>
