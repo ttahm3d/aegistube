d9c5b03dc37ed1bc8ab6f6d1d14630f188c95fb6
ttahm3d <ttahm3d@gmail.com>
2022-04-04 23:18:46 +0530

    feat: created filters

---
+++ src/pages/explore/VideoListing/Filters/Filters.jsx
@@ -0,0 +1,56 @@
+import { useVideos } from "../../../../context/videos";
+import { Button } from "../../../../components";
+import styles from "./Filters.module.css";
+
+export default function () {
+  const { category, changeCategory } = useVideos();
+
+  const filterBlocks = [
+    {
+      id: 1,
+      text: "All",
+    },
+    {
+      id: 2,
+      text: "True Sight",
+    },
+    {
+      id: 3,
+      text: "The International",
+    },
+    {
+      id: 4,
+      text: "Best Games",
+    },
+    {
+      id: 5,
+      text: "PRO POV Highlights",
+    },
+  ];
+
+  return (
+    <div className={`${styles.filters__container} p-y-1 p-x-1 m-y-1`}>
+      <div className={styles.filters}>
+        {filterBlocks.map((filter) => (
+          <>
+            <div
+              key={filter.id}
+              className={`${styles.filter} ${
+                category === filter.text && styles.filter__active
+              }`}
+              onClick={() => changeCategory(filter.text)}>
+              {filter.text}
+            </div>
+          </>
+        ))}
+        <div className={styles.reset__button}>
+          <Button
+            variant="secondary"
+            text="Reset"
+            onClick={() => changeCategory("All")}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
