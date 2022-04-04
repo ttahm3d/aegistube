2ff74f482bde42c8066b13e469c982311d9087c7
ttahm3d <ttahm3d@gmail.com>
2022-04-04 23:14:07 +0530

    feat: moved context and BrowserRouter outside

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
 import { useState } from "react";
-import { BrowserRouter } from "react-router-dom";
 import { Footer, Header, Sidebar } from "./components";
 import Router from "./router/Router";
 import { ToastContainer } from "react-toastify";
-import { AuthProvider } from "./context/auth";
 
 function App() {
   const [showSidebar, setShowSidebar] = useState(false);
-
   const toggleSidebar = () => setShowSidebar((s) => !s);
 
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <div className="page">
-          <Header toggleSidebar={toggleSidebar} />
-          <div className="container">
-            <div className="main-container">
-              <Sidebar
-                showSidebar={showSidebar}
-                toggleSidebar={toggleSidebar}
-              />
-              <Router />
-            </div>
-          </div>
-          <Footer />
-          <ToastContainer theme="dark" />
+    <div className="page">
+      <Header toggleSidebar={toggleSidebar} />
+      <div className="container">
+        <div className="main-container">
+          <Sidebar showSidebar={showSidebar} toggleSidebar={toggleSidebar} />
+          <Router />
         </div>
-      </AuthProvider>
-    </BrowserRouter>
+      </div>
+      <Footer />
+      <ToastContainer theme="dark" />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-
+import { AuthProvider } from "./context/auth";
+import { BrowserRouter } from "react-router-dom";
+import { VideosProvider } from "./context/videos";
 // Call make Server
 makeServer();
 
 const root = ReactDOMClient.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <VideosProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </VideosProvider>
+  </BrowserRouter>
+);
