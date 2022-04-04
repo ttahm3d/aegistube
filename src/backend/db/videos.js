0fc0059adade0bb50de83ea1fcd9f306c7e9452b
ttahm3d <ttahm3d@gmail.com>
2022-04-04 15:38:51 +0530

    feat: added ti videos

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -61,14 +61,141 @@ export const videos = [
   {
     _id: uuid(),
     videoId: "gHEPh8BaIbM",
-    title: "True Sight : The Kiev Major Grand Finals",
+    title: "TI 10 Grand Finals - PSG LGD Vs Team Spirit",
     description:
-      "TRUE SIGHT is a documentary series that takes you behind the scenes of the journeys of professional Dota 2 teams. This episode ...",
-    channelTitle: "dota2",
+      "Team Spirit Vs PSG LGD B05 Grand Finals of The International 10 for a prize money of $18,000,000",
+    channelTitle: "DotA Digest",
     channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
-    thumbnail: "https://i.ytimg.com/vi/GgAAPPf4z00/hqdefault.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti10.webp",
     views: 9727930,
     likes: 800249,
-    category: "True Sight",
+    category: "The International",
+  },
+  {
+    _id: uuid(),
+    videoId: "GRXAaebSCOc",
+    title: "TI 9 Grand Finals - Team Liquid Vs OG",
+    description:
+      "Team Liquid Vs OG Grand Finals of The International 9 for a prize money of $16,000,000",
+    channelTitle: "DotA Digest",
+    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti09.webp",
+    views: 9727930,
+    likes: 800249,
+    category: "The International",
+  },
+  {
+    _id: uuid(),
+    videoId: "eLxa8k_rdwg",
+    title: "TI 8 Grand Finals - PSG LGD Vs OG",
+    description:
+      "PSG LGD Vs OG Grand Finals of The International 8 for a prize money of $11,500,000",
+    channelTitle: "DotA Digest",
+    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti08.webp",
+    views: 9727930,
+    likes: 800249,
+    category: "The International",
+  },
+  {
+    _id: uuid(),
+    videoId: "eLxa8k_rdwg",
+    title: "TI 7 Grand Finals - Team Liquid vs NewBee",
+    description:
+      "Team Liquid vs NewBee Grand Finals of The International 7 for a prize money of $11,000,000",
+    channelTitle: "DotA Digest",
+    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti07.webp",
+    views: 9727930,
+    likes: 800249,
+    category: "The International",
+  },
+  {
+    _id: uuid(),
+    videoId: "cfHp96Y7GzI",
+    title: "TI 6 Grand Finals - Wings vs Digital Chaos",
+    description:
+      "Wings vs Digital Chaos Grand Finals of The International 6 for a prize money of $9,000,000",
+    channelTitle: "DotA Digest",
+    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti06.webp",
+    views: 9727930,
+    likes: 800249,
+    category: "The International",
+  },
+  {
+    _id: uuid(),
+    videoId: "cXLfDYrcj4g",
+    title: "TI 5 Grand Finals - Evil Geniuses vs CDEC",
+    description:
+      "EHOME vs Vici Gaming Grand Finals of The International 6 for a prize money of $6,600,000",
+    channelTitle: "DotA Digest",
+    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti05.webp",
+    views: 9727930,
+    likes: 800249,
+    category: "The International",
+  },
+  {
+    _id: uuid(),
+    videoId: "y2DyIWdgN7Y",
+    title: "TI 4 Grand Finals - Newbee vs Vici Gaming",
+    description:
+      "EHOME vs Vici Gaming Grand Finals of The International 6 for a prize money of $5,000,000",
+    channelTitle: "DotA Digest",
+    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti04.webp",
+    views: 9727930,
+    likes: 800249,
+    category: "The International",
+  },
+  {
+    _id: uuid(),
+    videoId: "IfOugI2ccUU",
+    title: "TI 3 Grand Finals - Alliance vs Natus Vincere",
+    description:
+      "EHOME vs Vici Gaming Grand Finals of The International 6 for a prize money of $1,400,000",
+    channelTitle: "DotA Digest",
+    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti03.webp",
+    views: 9727930,
+    likes: 800249,
+    category: "The International",
+  },
+  {
+    _id: uuid(),
+    videoId: "3_juIE5nfI4",
+    title: "TI 2 Grand Finals - Natus Vincere vs Invictus Gaming",
+    description:
+      "EHOME vs Vici Gaming Grand Finals of The International 6 for a prize money of $1,000,000",
+    channelTitle: "DotA Digest",
+    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti02.webp",
+    views: 9727930,
+    likes: 800249,
+    category: "The International",
+  },
+  {
+    _id: uuid(),
+    videoId: "hrEaaRLF-H4",
+    title: "TI 1 Grand Finals - EHOME vs Natus Vincere",
+    description:
+      "EHOME vs Vici Gaming Grand Finals of The International 6 for a prize money of $1,000,000",
+    channelTitle: "DotA Digest",
+    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649061350/AegisTube/Thumbnails/ti01.webp",
+    views: 9727930,
+    likes: 800249,
+    category: "The International",
   },
 ];
