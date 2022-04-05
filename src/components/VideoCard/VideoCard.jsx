84e3930a86606b0fb9f06cc7ebf73b2b75371eaf
ttahm3d <ttahm3d@gmail.com>
2022-04-05 21:35:54 +0530

    feat: moved video card to commone comps

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 export default function ({ video }) {
   const { _id, videoId, title, description, thumbnail } = video;
   return (
-    <Link to={`/video/${videoId}`}>
-      <div className={styles.card}>
+    <div className={styles.card}>
+      <Link to={`/video/${videoId}`}>
         <div>
           <img src={thumbnail} alt={title} className={styles.card__thumbnail} />
         </div>
         <div className={styles.card__description}>
           <div className={styles.card__title}>{title}</div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
