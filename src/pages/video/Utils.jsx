f101e78c57a27f0c10ce224c7e3389b71151cb3f
ttahm3d <ttahm3d@gmail.com>
2022-04-06 15:50:20 +0530

    feat: created util file for all the local requred data

---
+++ src/pages/video/Utils.jsx
@@ -0,0 +1,10 @@
+const getVideoData = (id, videos) =>
+  videos.find((video) => video.videoId === id);
+
+const getRelatedVideos = (category, videos) =>
+  videos.filter((video) => video.category === category);
+
+const isVideoLiked = (video, likedVideos) =>
+  likedVideos.some((likedVideo) => likedVideo._id === video._id);
+
+export { getRelatedVideos, getVideoData, isVideoLiked };
