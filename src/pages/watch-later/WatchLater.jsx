9c9a0ac0e48d3cc8891e335ebdb97a3af24a3b48
ttahm3d <ttahm3d@gmail.com>
2022-04-06 17:51:22 +0530

    style - changed some styles

--- src/pages/watch-later/WatchLater.jsx
+++ src/pages/watch-later/WatchLater.jsx
@@ -9,42 +9,11 @@ export default function () {
 
   const { watchLater } = useVideos();
 
-  const test = [
-    {
-      _id: "414be1c8-f627-4d94-aea0-970d4e99b82d",
-      videoId: "oWBDZo3axYg",
-      title: "True Sight : The International 2017 Finals",
-      description:
-        "TRUE SIGHT is a documentary series that takes you behind the scenes of the journeys of professional Dota 2 teams. This episode ...",
-      channelTitle: "dota2",
-      channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
-      thumbnail: "https://i.ytimg.com/vi/oWBDZo3axYg/hqdefault.jpg",
-      views: 13075555,
-      likes: 314111,
-      category: "True Sight",
-      id: "3",
-    },
-    {
-      _id: "e3900c4d-0e55-4cda-b91e-f8bccb702c99",
-      videoId: "ceQ2XFS1tUo",
-      title: "True Sight : The International 2019 Finals",
-      description:
-        "TRUE SIGHT is a documentary series that takes you behind the scenes of the journeys of professional Dota 2 teams. This episode ...",
-      channelTitle: "dota2",
-      channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
-      thumbnail: "https://i.ytimg.com/vi/ceQ2XFS1tUo/hqdefault.jpg",
-      views: 9848467,
-      likes: 322230,
-      category: "True Sight",
-      id: "2",
-    },
-  ];
-
   return (
     <div>
       <div className="content-container">
         {watchLater?.length === 0 ? (
-          <Empty text="Looks like there are no videos that you like. Lets change that. Visit below link" />
+          <Empty text="There are no videos to be 'Watched Later'. Visit the below link and add some to watch later " />
         ) : (
           <div className={styles.watchlater__container}>
             <div className={styles.watchlater__info}>
