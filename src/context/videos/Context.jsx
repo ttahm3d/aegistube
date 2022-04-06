1554e2b152dd7a6757676418191517fde453d08b
ttahm3d <ttahm3d@gmail.com>
2022-04-06 15:48:23 +0530

    feat: created functions in context

--- src/context/videos/Context.jsx
+++ src/context/videos/Context.jsx
@@ -5,18 +5,24 @@ import {
   useState,
   useEffect,
 } from "react";
+import { useLocalStorage } from "../../hooks";
 import axios from "axios";
 import { filterByCategory, getResultantVideos } from "./utils";
 import { videosReducer } from "./Reducer";
+import Toast from "../../components/Toast/Toast";
+import { useAuth } from "../auth";
 
 const VideosContext = createContext();
 
 const VideosProvider = ({ children }) => {
   const [videoState, videoDispatch] = useReducer(videosReducer, {
     category: "All",
+    likedVideos: [],
   });
-
+  const [token] = useLocalStorage("video-lib-user-token");
   const [videos, setVideos] = useState([]);
+
+  const { isLoggedIn } = useAuth();
 
   useEffect(() => {
     (async () => {
@@ -40,11 +46,79 @@ const VideosProvider = ({ children }) => {
     });
   };
 
+  const addToLikedVideos = async (video) => {
+    if (isLoggedIn) {
+      if (
+        videoState.likedVideos.some(
+          (likedVideo) => likedVideo._id === video._id
+        )
+      ) {
+        removeFromLikedVideos(video);
+      } else {
+        try {
+          const response = await axios.post(
+            `/api/user/likes`,
+            { video },
+            {
+              headers: {
+                authorization: token,
+              },
+            }
+          );
+          if (response.status === 201) {
+            videoDispatch({
+              type: "ADD_TO_LIKED",
+              payload: response?.data?.likes,
+            });
+            Toast({
+              type: "success",
+              message: `${video.title} has been added to liked videos`,
+            });
+          }
+        } catch (e) {
+          console.error(e);
+          Toast({
+            type: "error",
+            message: "Something went wrong. Try again.",
+          });
+        }
+      }
+    }
+  };
+
+  const removeFromLikedVideos = async (video) => {
+    try {
+      const response = await axios.delete(`/api/user/likes/${video._id}`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      if (response.status === 200) {
+        videoDispatch({
+          type: "REMOVE_FROM_LIKED",
+          payload: response?.data?.likes,
+        });
+        Toast({
+          type: "info",
+          message: `${video.title} has been removed from Liked Videost`,
+        });
+      }
+    } catch (e) {
+      Toast({
+        type: "error",
+        message: "Something went wrong. Try again.",
+      });
+    }
+  };
+
   return (
     <VideosContext.Provider
       value={{
         videos: videosToShow,
         category: videoState.category,
+        likedVideos: videoState.likedVideos,
+        addToLikedVideos,
+        removeFromLikedVideos,
         changeCategory,
         videoState,
         videoDispatch,
--- src/context/videos/Reducer.jsx
+++ src/context/videos/Reducer.jsx
@@ -2,6 +2,9 @@ const videosReducer = (state, { type, payload }) => {
   switch (type) {
     case "FILTER_BY_CATEGORY":
       return { ...state, category: payload };
+    case "ADD_TO_LIKED":
+    case "REMOVE_FROM_LIKED":
+      return { ...state, likedVideos: payload };
     default:
       return state;
   }
