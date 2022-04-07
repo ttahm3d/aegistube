8eca76308594aaa82d9e21fe47d2273ceac219b9
ttahm3d <ttahm3d@gmail.com>
2022-04-07 19:55:53 +0530

    feat: changes to video card

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -2,7 +2,7 @@ import styles from "./VideoCard.module.css";
 import { Link } from "react-router-dom";
 
 export default function ({ video }) {
-  const { _id, videoId, title, thumbnail } = video;
+  const { _id, videoId, title, thumbnail, channelIcon, channelTitle } = video;
   return (
     <div className={styles.card} key={_id}>
       <Link to={`/video/${videoId}`}>
@@ -10,7 +10,17 @@ export default function ({ video }) {
           <img src={thumbnail} alt={title} className={styles.card__thumbnail} />
         </div>
         <div className={styles.card__description}>
-          <div className={styles.card__title}>{title}</div>
+          <div className={styles.card__channel__icon__container}>
+            <img
+              src={channelIcon}
+              alt={channelTitle}
+              className={styles.card__channel__icon}
+            />
+          </div>
+          <div className={styles.card__video__info}>
+            <div className={styles.card__video__title}>{title}</div>
+            <div className={styles.card__video__publisher}>{channelTitle}</div>
+          </div>
         </div>
       </Link>
     </div>
