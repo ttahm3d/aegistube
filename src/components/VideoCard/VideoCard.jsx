976844b349a2f0ee81d1b0fd0da725b99b2dc52a
ttahm3d <ttahm3d@gmail.com>
2022-04-07 19:07:07 +0530

    refactor; reorderd code

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -2,17 +2,17 @@ import styles from "./VideoCard.module.css";
 import { Link } from "react-router-dom";
 
 export default function ({ video }) {
-  const { _id, videoId, title, description, thumbnail } = video;
+  const { _id, videoId, title, thumbnail } = video;
   return (
-    <Link to={`/video/${videoId}`}>
-      <div className={styles.card}>
+    <div className={styles.card} key={_id}>
+      <Link to={`/video/${videoId}`}>
         <div>
           <img src={thumbnail} alt={title} className={styles.card__thumbnail} />
         </div>
         <div className={styles.card__description}>
           <div className={styles.card__title}>{title}</div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
