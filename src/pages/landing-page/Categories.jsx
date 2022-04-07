ff7492bc66f3f11b54564e529e88e2ee00226229
ttahm3d <ttahm3d@gmail.com>
2022-04-07 17:36:47 +0530

    feat: added navigation from homepage to explore

--- src/pages/landing-page/Categories.jsx
+++ src/pages/landing-page/Categories.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./LandingPage.module.css";
+import { useVideos } from "../../context/videos";
+import { useNavigate } from "react-router-dom";
 
 export default function () {
   const [categories, setCategories] = useState([]);
-
+  const { changeCategory } = useVideos();
+  const navigate = useNavigate();
   useEffect(() => {
     (async () => {
       try {
@@ -16,12 +19,20 @@ export default function () {
     })();
   }, []);
 
+  const handleClick = (categoryName) => {
+    changeCategory(categoryName);
+    navigate("/explore");
+  };
+
   return (
     <div className="p-x-2 p-y-2">
       <div className={styles.categories__heading}>Browse Categories</div>
       <div className={`${styles.categories} p-y-1`}>
         {categories.map(({ id, name, url, description }) => (
-          <div key={id} className={styles.category__card}>
+          <div
+            key={id}
+            className={styles.category__card}
+            onClick={() => handleClick(name)}>
             <div>
               <img
                 src={url}
