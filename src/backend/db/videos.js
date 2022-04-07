5078baa2a658b2f4a6af155bb07afdf791263f9e
ttahm3d <ttahm3d@gmail.com>
2022-04-07 20:06:04 +0530

    feat: added images in db for channel

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -13,7 +13,8 @@ export const videos = [
     description:
       "TRUE SIGHT is a documentary series that takes you behind the scenes of the journeys of professional Dota 2 teams. This episode ...",
     channelTitle: "dota2",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338262/AegisTube/Thumbnails/channels/dota.jpg",
     thumbnail: "https://i.ytimg.com/vi/Bv4CqIxqTMA/hqdefault.jpg",
     views: 9055621,
     likes: 164380,
@@ -26,7 +27,8 @@ export const videos = [
     description:
       "TRUE SIGHT is a documentary series that takes you behind the scenes of the journeys of professional Dota 2 teams. This episode ...",
     channelTitle: "dota2",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338262/AegisTube/Thumbnails/channels/dota.jpg",
     thumbnail: "https://i.ytimg.com/vi/ceQ2XFS1tUo/hqdefault.jpg",
     views: 9848467,
     likes: 322230,
@@ -39,7 +41,8 @@ export const videos = [
     description:
       "TRUE SIGHT is a documentary series that takes you behind the scenes of the journeys of professional Dota 2 teams. This episode ...",
     channelTitle: "dota2",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338262/AegisTube/Thumbnails/channels/dota.jpg",
     thumbnail: "https://i.ytimg.com/vi/oWBDZo3axYg/hqdefault.jpg",
     views: 13075555,
     likes: 314111,
@@ -52,7 +55,8 @@ export const videos = [
     description:
       "TRUE SIGHT is a documentary series that takes you behind the scenes of the journeys of professional Dota 2 teams. This episode ...",
     channelTitle: "dota2",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338262/AegisTube/Thumbnails/channels/dota.jpg",
     thumbnail: "https://i.ytimg.com/vi/GgAAPPf4z00/hqdefault.jpg",
     views: 9727930,
     likes: 800249,
@@ -65,7 +69,8 @@ export const videos = [
     description:
       "Team Spirit Vs PSG LGD B05 Grand Finals of The International 10 for a prize money of $18,000,000",
     channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338262/AegisTube/Thumbnails/channels/dotadigest.jpg",
     thumbnail:
       "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti10.webp",
     views: 9727930,
@@ -78,8 +83,9 @@ export const videos = [
     title: "TI 9 Grand Finals - Team Liquid Vs OG",
     description:
       "Team Liquid Vs OG Grand Finals of The International 9 for a prize money of $16,000,000",
-    channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelTitle: "Noobfromua",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338262/AegisTube/Thumbnails/channels/noobfromua.jpg",
     thumbnail:
       "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti09.webp",
     views: 9727930,
@@ -92,8 +98,9 @@ export const videos = [
     title: "TI 8 Grand Finals - PSG LGD Vs OG",
     description:
       "PSG LGD Vs OG Grand Finals of The International 8 for a prize money of $11,500,000",
-    channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelTitle: "Noobfromua",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338262/AegisTube/Thumbnails/channels/noobfromua.jpg",
     thumbnail:
       "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti08.webp",
     views: 9727930,
@@ -102,12 +109,13 @@ export const videos = [
   },
   {
     _id: uuid(),
-    videoId: "eLxa8k_rdwg",
+    videoId: "Jwg7F8BX2p0",
     title: "TI 7 Grand Finals - Team Liquid vs NewBee",
     description:
       "Team Liquid vs NewBee Grand Finals of The International 7 for a prize money of $11,000,000",
-    channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelTitle: "Noobfromua",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338380/AegisTube/Thumbnails/channels/noobfromua.jpg",
     thumbnail:
       "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti07.webp",
     views: 9727930,
@@ -120,8 +128,9 @@ export const videos = [
     title: "TI 6 Grand Finals - Wings vs Digital Chaos",
     description:
       "Wings vs Digital Chaos Grand Finals of The International 6 for a prize money of $9,000,000",
-    channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelTitle: "Noobfromua",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338380/AegisTube/Thumbnails/channels/noobfromua.jpg",
     thumbnail:
       "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti06.webp",
     views: 9727930,
@@ -134,8 +143,9 @@ export const videos = [
     title: "TI 5 Grand Finals - Evil Geniuses vs CDEC",
     description:
       "EHOME vs Vici Gaming Grand Finals of The International 6 for a prize money of $6,600,000",
-    channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelTitle: "Noobfromua",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338380/AegisTube/Thumbnails/channels/noobfromua.jpg",
     thumbnail:
       "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti05.webp",
     views: 9727930,
@@ -148,8 +158,9 @@ export const videos = [
     title: "TI 4 Grand Finals - Newbee vs Vici Gaming",
     description:
       "EHOME vs Vici Gaming Grand Finals of The International 6 for a prize money of $5,000,000",
-    channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelTitle: "Mogul HD",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649339818/AegisTube/Thumbnails/channels/magul.jpg",
     thumbnail:
       "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti04.webp",
     views: 9727930,
@@ -162,8 +173,9 @@ export const videos = [
     title: "TI 3 Grand Finals - Alliance vs Natus Vincere",
     description:
       "EHOME vs Vici Gaming Grand Finals of The International 6 for a prize money of $1,400,000",
-    channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelTitle: "Noobfromua",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338380/AegisTube/Thumbnails/channels/noobfromua.jpg",
     thumbnail:
       "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti03.webp",
     views: 9727930,
@@ -176,8 +188,9 @@ export const videos = [
     title: "TI 2 Grand Finals - Natus Vincere vs Invictus Gaming",
     description:
       "EHOME vs Vici Gaming Grand Finals of The International 6 for a prize money of $1,000,000",
-    channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelTitle: "Noobfromua",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338380/AegisTube/Thumbnails/channels/noobfromua.jpg",
     thumbnail:
       "https://res.cloudinary.com/dut75albw/image/upload/v1649061539/AegisTube/Thumbnails/ti02.webp",
     views: 9727930,
@@ -190,8 +203,9 @@ export const videos = [
     title: "TI 1 Grand Finals - EHOME vs Natus Vincere",
     description:
       "EHOME vs Vici Gaming Grand Finals of The International 6 for a prize money of $1,000,000",
-    channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelTitle: "Noobfromua",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338380/AegisTube/Thumbnails/channels/noobfromua.jpg",
     thumbnail:
       "https://res.cloudinary.com/dut75albw/image/upload/v1649061350/AegisTube/Thumbnails/ti01.webp",
     views: 9727930,
@@ -204,24 +218,40 @@ export const videos = [
     title: "Team Secret vs Mineski - TI 9",
     description:
       "Team Secret vs Mineski - BO3 series. Game 2. Secret are 1 - 0 up and Mineski are defending for thier chances in the Tournament",
-    channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelTitle: "DotA 2 Archive",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649339898/AegisTube/Thumbnails/channels/dota2archive.jpg",
     thumbnail:
-      "https://res.cloudinary.com/dut75albw/image/upload/v1649061350/AegisTube/Thumbnails/ti01.webp",
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649340874/AegisTube/Thumbnails/secretmineskiti9.webp",
     views: 9727930,
     likes: 800249,
     category: "Best Games",
   },
   {
     _id: uuid(),
-    videoId: "Th3ayeM1HQM",
+    videoId: "orOLWPztMmQ",
     title: "Team Liquid vs LGD - TI 9 Lower Bracket Finals",
     description:
       "Arguably the best of game of Entire TI9. Team Liquid after dropping down in LB and completing their run through it finally meet the Chinese DOTA Gods PSG LGD for spot in Final VS OG",
-    channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelTitle: "DotA 2 Archive",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649339898/AegisTube/Thumbnails/channels/dota2archive.jpg",
     thumbnail:
-      "https://res.cloudinary.com/dut75albw/image/upload/v1649061350/AegisTube/Thumbnails/ti01.webp",
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649340820/AegisTube/Thumbnails/liquidlgdti9.webp",
+    views: 9727930,
+    likes: 800249,
+    category: "Best Games",
+  },
+  {
+    _id: uuid(),
+    videoId: "bMPc2MuJHOw",
+    title: "Team Liquid vs VP - TI 7 Lower Bracket Semi Finals",
+    description: "The longest game ever in the history of The Internationals",
+    channelTitle: "DotA 2 Archive",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649339898/AegisTube/Thumbnails/channels/dota2archive.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649340750/AegisTube/Thumbnails/liquidvpti7.webp",
     views: 9727930,
     likes: 800249,
     category: "Best Games",
@@ -233,9 +263,24 @@ export const videos = [
     description:
       "Known as M-God in the DotA community, catch the best plays of Miracle",
     channelTitle: "DotA Digest",
-    channelId: "UCTQKT5QqO3h7y32G8VzuySQ",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338408/AegisTube/Thumbnails/channels/holyhexor.jpg",
     thumbnail:
-      "https://res.cloudinary.com/dut75albw/image/upload/v1649061350/AegisTube/Thumbnails/ti01.webp",
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649340967/AegisTube/Thumbnails/miracle.webp",
+    views: 9727930,
+    likes: 800249,
+    category: "PRO POV Highlights",
+  },
+  {
+    _id: uuid(),
+    videoId: "TTJh2Fb7KWk",
+    title: "Best of Sumail",
+    description: "Sumail's best plays",
+    channelTitle: "DotA Digest",
+    channelIcon:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649338408/AegisTube/Thumbnails/channels/holyhexor.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/dut75albw/image/upload/v1649340310/AegisTube/Thumbnails/sumail.webp",
     views: 9727930,
     likes: 800249,
     category: "PRO POV Highlights",
